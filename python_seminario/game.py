367c2bca5ed6a616de5f9cb9ebda101a9d47b739
Noe Depenta <noedepenta2@gmail.com>
2024-03-21 08:19:24 -0300

    Agregando niveles de dificultad

--- python_seminario/game.py
+++ python_seminario/game.py
@@ -1,14 +1,37 @@
 import random
-words = ["python", "programación", "computadora", "código", "desarrollo",
-"inteligencia"]
+niveles =""" Seleccione el nivel de dificultad:
+            1.Fácil
+            2.Media
+            3.Díficil
+         """
+vocales= ["a","e","i","o","u"]       
+words = ["python", "programación", "computadora", "código", "desarrollo","inteligencia"]
 secret_word = random.choice(words)
 max_attempts = 3
 guessed_letters = []
 print("¡Bienvenido al juego de adivinanzas!")
+letters=[]
+print (niveles)
+nivel=int(input("Ingrese la dificultad: "))
+if nivel==1:
+    for letter in secret_word:
+        if letter in vocales:
+            guessed_letters.append(letter)
+            letters.append(letter)
+        else:
+            letters.append("_")
+    word_displayed="".join(letters)
+elif nivel==2:
+    word_displayed = "_" * len(secret_word)
+    word_displayed=secret_word[0:1]+word_displayed[1:]
+    word_displayed=word_displayed[0:-1]+secret_word[-1]
+    guessed_letters.append(secret_word[0])
+    guessed_letters.append(secret_word[-1])
+elif nivel==3:
+    word_displayed = "_" * len(secret_word)
 print("Estoy pensando en una palabra. ¿Puedes adivinar cuál es?")
-print ("Tienes 3 errores permitidos");
-word_displayed = "_" * len(secret_word)
 print(f"Palabra: {word_displayed}")
+#CODIGO
 while max_attempts>0:
     letter = input("Ingresa una letra: ").lower()
     if letter in guessed_letters:
@@ -19,7 +42,7 @@ while max_attempts>0:
         print("¡Bien hecho! La letra está en la palabra.")
     elif letter not in secret_word and letter:
         print("Lo siento, la letra no está en la palabra.")
-        max_attempts=max_attempts-1
+        max_attempts-=1
     elif not letter:
         print ("Error no ha insertardo ninguna palabra")
     letters = []
